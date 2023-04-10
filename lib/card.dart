a771ecd7f373a2b10909acf88c04cae8b25644fa
floor777 <bvfwra@gmail.com>
2023-04-10 15:51:20 -0700

    Changed class name from card to flashcard to avoid overlap with existing library

---
+++ lib/card.dart
@@ -0,0 +1,12 @@
+class FlashCard {
+  String frontItem = '';
+  String backItem = '';
+  int intervalValue = 0;
+
+  FlashCard(
+      {required this.frontItem, required this.backItem, required this.intervalValue
+      }
+      );
+
+
+}
